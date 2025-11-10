39842db11d92c5d8bf269648d95fd2ef139d8787
Afuwape Babatunde <afutunde@gmail.com>
2025-11-10 22:58:56 +0100

    Setup: Complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import "./App.css";
+import { Button, HStack } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -28,6 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <HStack justifyContent="center" marginTop={10}>
+        <Button variant="surface">Click me</Button>
+        <Button variant="surface">Click me</Button>
+      </HStack>
     </>
   );
 }
