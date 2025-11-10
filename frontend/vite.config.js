e3d84af625f15299dd227452120bead054deac98
Afuwape Babatunde <afutunde@gmail.com>
2025-11-10 22:45:17 +0100

    Setup: Vite config file configuration to set base as the repository folder name

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: mode === "production" ? "/V58-tier3-team-35/" : "/",
+  // base: "/V58-tier3-team-35/",
+}));
