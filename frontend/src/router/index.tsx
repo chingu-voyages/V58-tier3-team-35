7ba62e4048213f255c3dee2c6f238eb6b798c615
Afuwape Babatunde <afutunde@gmail.com>
2025-11-12 02:28:41 +0100

    Fix: Corrected the base route issue in production

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import routes from "./routes";
+const isProduction = import.meta.env.MODE === "production";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  basename: isProduction ? "/V58-tier3-team-35/" : "/",
+});
 
 export default router;
